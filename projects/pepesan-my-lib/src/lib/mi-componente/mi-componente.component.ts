098cecca9644aae6719f96d605bf99cb218cb166
David Vaquero <pepesan@gmail.com>
2025-05-27 12:20:22 +0200

    metido nuevo componente y actualizada la versión de la biblioteca standalone

---
+++ projects/pepesan-my-lib/src/lib/mi-componente/mi-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-mi-componente',
+  imports: [],
+  templateUrl: './mi-componente.component.html',
+  styleUrl: './mi-componente.component.css'
+})
+export class MiComponenteComponent {
+
+}
--- projects/pepesan-my-lib/src/public-api.ts
+++ projects/pepesan-my-lib/src/public-api.ts
@@ -4,3 +4,4 @@
 
 export * from './lib/pepesan-my-lib.service';
 export * from './lib/pepesan-my-lib.component';
+export * from './lib/mi-componente/mi-componente.component';
